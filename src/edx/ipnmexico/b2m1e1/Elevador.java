e2ccc13a1e37eaec70d908691a0e8852e6cb2d11
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-13 21:13:46 -0500

    Instancia dos objetos tipo "Elevador"
     - Agrega un constructor a la clase "Elevador" que le permitirá
       a la clase otorgarle valores por defecto a los atributos de
       la clase.
     - Instancia dos objetos de la clase "Elevador" una para uno
       pequeño y otro más grande.
    
    Sin embargo el problema que ocurre es que cada uno de los objetos
    instanciados tienen inicializados los mismos valores.

--- src/edx/ipnmexico/b2m1e1/Elevador.java
+++ src/edx/ipnmexico/b2m1e1/Elevador.java
@@ -6,6 +6,13 @@ public class Elevador {
 	public int pesoMaximo;
 	public int capacidadMaximaPersonas;
 	
+	//-> Constructor
+	public Elevador() {
+		//-> Inicializamos los atributos a través del constructor.
+		pesoMaximo = 400;
+		capacidadMaximaPersonas = 5;
+	}
+	
 	//-> Métodos
 	public int subirPiso( int pisoActual ) {
 		return pisoActual + 1;
--- src/edx/ipnmexico/b2m1e1/Principal.java
+++ src/edx/ipnmexico/b2m1e1/Principal.java
@@ -6,6 +6,12 @@ public class Principal {
 		//-> Creamos el primer objeto de tipo Elevador
 		//   para un elevador de baja capacidad
 		Elevador elevadorPequenio = new Elevador();
+		System .out .println( "Elevador pequeño \n - Peso Máximo: " + elevadorPequenio .pesoMaximo + "\n - Capacidad " + elevadorPequenio .capacidadMaximaPersonas );
+		
+		//-> Creamos el segundo objeto de tipo Elevador
+		//   para un elevador de más capacidad
+		Elevador elevadorMasGrande = new Elevador();
+		System .out .print( "Elevador grande \n - Peso Máximo: " + elevadorMasGrande .pesoMaximo + "\n - Capacidad " + elevadorMasGrande .capacidadMaximaPersonas );
 	}
 
 }
