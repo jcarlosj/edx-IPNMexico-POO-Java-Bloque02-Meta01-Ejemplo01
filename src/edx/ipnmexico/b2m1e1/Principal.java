34d553781261d3243bf6016f3f91ab529edbd2ed
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-13 20:58:31 -0500

    Instancia de la clase "Elevador"
     - Esta instancia permitirá el acceso a la clase sus atributos y
       funcionalidades.

--- src/edx/ipnmexico/b2m1e1/Principal.java
+++ src/edx/ipnmexico/b2m1e1/Principal.java
@@ -3,8 +3,9 @@ package edx.ipnmexico.b2m1e1;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//-> Creamos el primer objeto de tipo Elevador
+		//   para un elevador de baja capacidad
+		Elevador elevadorPequenio = new Elevador();
 	}
 
 }
