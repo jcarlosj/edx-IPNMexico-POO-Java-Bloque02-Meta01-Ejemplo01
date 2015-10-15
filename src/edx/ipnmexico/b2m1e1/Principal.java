d3b62c4636354a6d6fddf4d6439e0f35d3192e4c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-15 10:59:38 -0500

    Implementa el concepto de polimorfismo.
     - Elimina el llamado al método "desplazarse()" en cada
       uno de los objetos ("animal", "ave", "pez", "mt")
     - Crea el método estático "DesplazarAnimal()" el cual
       recibe como argumento un objeto de la clase "Animal"
     - Posterior a esto en el método "main()" se llama el
       método "DesplazarAnimal()" pasandole cada uno de los
       objetos ("animal", "ave", "pez", "mt")
    
    Vemos como apesar de que cada uno de los objetos que pasamos
    al método "DesplazarAnimal()" son aparentemente diferentes
    estos son interpretados como de tipo animal, esto pasa por que
    al extenderse cada uno de ellos de la clase "Animal" en su
    contexto no solo hereda los atributos y los métodos que hay
    en su clase padre, si no que también heredan el tipo que posee,
    lo que significa que todos son también de tipo "Animal"

--- src/edx/ipnmexico/b2m1e1/Principal.java
+++ src/edx/ipnmexico/b2m1e1/Principal.java
@@ -6,16 +6,25 @@ public class Principal {
 		
 		//-> Clase padre
 		Animal animal = new Animal();
-		animal .desplazarse();	//: Método original en la clase padre.
 		
 		//-> Sub-Clases, Clases hijas o Clases que heredan de la clase padre
-		Ave ave = new Ave();
-		ave .desplazarse();		//: Accede al método sobre escrito en la clase "Ave"
-		
+		Ave ave = new Ave();								
 		Pez pez = new Pez();
-		pez .desplazarse();		//: Accede al método sobre escrito en la clase "Pez"
-		
 		MamiferoTerrestre mt = new MamiferoTerrestre();
-		mt .desplazarse();		//: Accede al método sobre escrito en la clase "MamiferoTerrestre"
+		
+		//-> Realizamos el desplazamiento pasando cada uno de los objetos
+		DesplazarAnimal( animal ); 		//: El objeto "animal" es de tipo "Animal"
+		DesplazarAnimal( ave ); 		//: El objeto "ave" es de tipo "Ave" y de tipo "Animal"
+		DesplazarAnimal( pez ); 		//: El objeto "pez" es de tipo "Pez" y de tipo "Animal"
+		DesplazarAnimal( mt ); 			//: El objeto "mt" es de tipo "MamiferoTerrestre" y de tipo "Animal"
 	}	
+	
+	//-> Muestra cada tipo de desplazamiento ya que aunque cada objeto que se le pasa
+	//   es de tipo "Animal" por herencia, hace una generalización para poder acceder 
+	//   a sus métodos
+	public static void DesplazarAnimal( Animal animal ) {
+		//-> Realiza la generalización y trata el objeto solo como de tipo animal.
+		//   Osea con el tipo que trae por herencia de su padre.
+		animal .desplazarse();
+	}
 }
